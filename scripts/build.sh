675f13e62a49fa63b895042bcf11d98c63c7297f
ricky <rickycontact9@gmail.com>
2025-08-27 12:17:47 -0500

    Fix onnx file name

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ cp src/*.html dist
 npx tailwindcss -i ./src/input.css -o ./dist/style.css
 npx esbuild src/demo.tsx --bundle --outfile=dist/demo.js --format=esm
 cp \
-    node_modules/@ricky0123/vad-web/dist/silero_vad.onnx \
+    node_modules/@ricky0123/vad-web/dist/silero_vad_legacy.onnx \
     node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js \
     node_modules/onnxruntime-web/dist/*.wasm \
     dist
